3a6cc6814f0abff57f39cb90e7e84e78c9cdb21f
Yuze Fu <i@xfox.me>
2024-01-15 15:52:31 +0900

    test: add more test cases

--- test/exception.cpp
+++ test/exception.cpp
@@ -1,10 +1,6 @@
 #include "eff-unwind.hpp"
 #include "fmt/core.h"
 
-struct RAII {
-  ~RAII() {}
-};
-
 struct Exception : public effect<uint64_t, uint64_t> {};
 
 with_effect<int, Exception> foobar() {
@@ -20,30 +16,18 @@ uint64_t has_handler() {
         return handler_return<uint64_t>(72);
       });
   int num = 42;
-  // RAII raii;
   int ret = foobar().value;
   return 24;
 }
 
 void test() {
-  // FIXME: if Release mode is specified, cannot be handled correctly
-  // need to tell the C++ optimizer that return value is not a constant
   auto val = has_handler();
 }
 
 int main() {
   int MAX = 1'000'000;
-  auto begin = std::chrono::high_resolution_clock::now();
-
   for (int i = 0; i < MAX; i++) {
     test();
   }
-  auto end = std::chrono::high_resolution_clock::now();
-  fmt::println(
-      "{}ns    ({}ns per iteration)",
-      std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count(),
-      (int)(std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin)
-                .count() /
-            MAX));
   return 0;
 }
